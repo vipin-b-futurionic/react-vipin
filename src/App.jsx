3ac9b82d3a9ed4647f9b8ae58942ddc6938ca2c0
vipin-b-futurionic <vipin.bhaskar@futurionic.com>
2025-04-01 23:39:30 +0530

    import,export componenents

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import {AnimeSeries, Footer} from "./components/AnimeSeries"
+
+export const App = () => {
+  return (
+    <div>
+      <AnimeSeries /><br />
+      <AnimeSeries /><br />
+      <AnimeSeries /><br />
+      <AnimeSeries /><br />
+      <AnimeSeries /><br />
+      <Footer />
+    </div>
+
+
+   
+  )
+  
+}
+
+
---
+++ src/components/AnimeSeries.jsx
@@ -0,0 +1,29 @@
+export const AnimeSeries = () =>{
+    return (
+      <div>
+        <div>
+          <img src="ace.jpg" alt="One Piece ACE" width = "45%"  />
+        </div>
+        <h2>Anime: One Piece</h2>
+        <h3>Rating: 10/10</h3>
+        <p>
+          Portgas D. Ace, also known as Fire Fist Ace,
+          was one of the most legendary characters in
+          One Piece. As the sworn brother of Luffy and
+          Sabo, he carried the will of Gol D. Roger 
+          while forging his own path. His Mera Mera no Mi (Flame-Flame Fruit)
+          made him a fearsome pirate, earning him 
+          the title of the 2nd division commander of the 
+          Whitebeard Pirates.
+        </p>
+        <button>Watch Now</button>
+      </div>
+    )
+  }
+
+ // export default AnimeSeries
+
+export const Footer= () =>{
+  return (<p> copyright @Crunchyroll </p>)
+
+ }

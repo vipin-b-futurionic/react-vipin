5a529c20c96d1d9f5c2a92f359425e01ac243077
vipin-b-futurionic <vipin.bhaskar@futurionic.com>
2025-04-02 11:20:30 +0530

    using map function

--- src/components/AnimeSeries.jsx
+++ src/components/AnimeSeries.jsx
@@ -2,26 +2,35 @@ import seriesData from "../api/seriesData.json"
 
 const AnimeSeries = () =>{
     return (
-      <div>
-        <div>
-          <img 
-          src={seriesData[0].img_url} 
-          alt="qot.jpg"
-          width="40%"
-          height="40%"
-           />
-        </div>
+      <ul>
+        {seriesData.map( (curElem) => {
+          return (
+            <li key = {curElem.id}>
+              <div>
+                <img 
+                src={curElem.img_url} 
+                alt="qot.jpg"
+                width="40%"
+                height="40%"
+                />
+              </div>
 
-        <h2>Name: {seriesData[0].name}</h2>
-        <h3>Rating:{seriesData[0].rating} </h3>
-        <p> Summary: {seriesData[0].description}</p>
-        <p> genre: {seriesData[0].genre}</p>
-        <p> cast: {seriesData[0].cast}</p>
-        <a href={seriesData[0].watch_url} target="_blank">
-          <button>Watch Now</button>
-        </a>
+              <h2>Name: {curElem.name}</h2>
+              <h3>Rating:{curElem.rating} </h3>
+              <p> Summary: {curElem.description}</p>
+              <p> genre: {curElem.genre}</p>
+              <p> cast: {curElem.cast}</p>
+              <a href={curElem.watch_url} target="_blank">
+              <button>Watch Now</button>
+              
+              </a>
 
-      </div>
+            </li>
+          )
+
+        })}
+        
+      </ul>
     )
   }
 
